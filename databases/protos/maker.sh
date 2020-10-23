b423356a1c8da9f7d7e024fe12d878ed28df1efe
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-23 11:46:04 +0000

    add casndra DB

---
+++ databases/protos/maker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# create access token
+# protoc --go_out=. --go-grpc_out=. --go-grpc_opt=paths=import --go_opt=paths=import databases/protos/accesstokenpb/accesstokenpb.proto 
+protoc --go_out=. --go-grpc_out=. --go-grpc_opt=paths=source_relative --go_opt=paths=source_relative databases/protos/accesstokenpb/accesstokenpb.proto 
+
+# protoc --go_out=plugins=grpc:. databases/protos/accesstokenpb/accesstokenpb.proto 
