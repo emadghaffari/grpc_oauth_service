de974a342a29657a9da7019dc981b948a127cbb9
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-23 11:45:05 +0000

    [feature] add random generator string

---
+++ utils/rand/rand.go
@@ -0,0 +1,28 @@
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+  rand.NewSource(time.Now().UnixNano()))
+
+//StringWithCharset func
+// create random string
+func StringWithCharset(length int, charset string) string {
+  b := make([]byte, length)
+  for i := range b {
+    b[i] = charset[seededRand.Intn(len(charset))]
+  }
+  return string(b)
+}
+
+// String func
+// create random string with len
+func String(length int) string {
+  return StringWithCharset(length, charset)
+}
